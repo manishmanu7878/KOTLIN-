d15ee80e2aea3b0c03b6a0a0b491fbc5004cb1ea
manishmanu7878 <manishmanu7878@gmail.com>
2021-09-28 10:11:37 +0530

    variables : kotlin

---
+++ kotlin1/app/src/main/java/com/manish/kotlin/variables.kt
@@ -0,0 +1,22 @@
+package com.manish.kotlin
+
+fun main() {
+    var age = 20
+    age = 30
+
+    val mr = 25
+
+    val st = "manish"
+    val c = 25
+    val db = 89.76
+    val ff = 56.2f
+    val boo = true
+    val lng: Long = 78885452105
+    val bt: Byte = 44
+
+    println("Addition = ${20 + 30}")
+    println("string = $st \n number = $c \n double = $db \n float = $ff \n boolean = ${boo} \n long = ${lng} \n byte = ${bt} ")
+    println("\n Square Root = ${ Math.sqrt(25.6) } ")
+
+
+}
