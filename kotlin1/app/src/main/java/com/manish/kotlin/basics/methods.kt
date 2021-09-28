0c3966921c143306bb5cad008c261593f96b3b79
manishmanu7878 <manishmanu7878@gmail.com>
2021-09-28 11:13:52 +0530

    Methods : kotlin

--- kotlin1/app/src/main/java/com/manish/kotlin/basics/methods.kt
+++ kotlin1/app/src/main/java/com/manish/kotlin/basics/methods.kt
@@ -13,9 +13,25 @@ fun rtntyp(): Unit {
     println("Just call")
 }
 
+fun nTimes(num: Int, n: Int) = n * num
+
+fun defaultValues(
+    num1: Int = 30,
+    num2: Int = 20
+): String {
+    return "Num1 = ${num1} , Num2 = ${num2}"
+
+}
+
 fun main() {
     val ab = simple()
     println("ab is $ab")
     println("Addition is ${addition(12, 3)}")
     rtntyp()
+
+    println("10 * 3 times is ${nTimes(10, 3)}")
+    println("default values() ${defaultValues()}")
+    println("defaultValues(89,56) ${defaultValues(89, 56)}")
+    println("defaultValues(num2 = 4555, num1= 10) ${defaultValues(num2 = 4555, num1 = 10)}")
+    println("defaultValue(num2=878) ${defaultValues(num2 = 878)}")
 }
