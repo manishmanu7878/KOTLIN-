6c474a9d95936085ed90a824a198f726bdf33043
manishmanu7878 <manishmanu7878@gmail.com>
2021-09-28 12:50:05 +0530

    function in function

--- kotlin1/app/src/main/java/com/manish/kotlin/basics/var-mat-adv.kt
+++ kotlin1/app/src/main/java/com/manish/kotlin/basics/var-mat-adv.kt
@@ -6,8 +6,27 @@ fun objDemo() {
         var os = "android"
     }
     println("dt is ${obj.dt} os is ${obj.os}")
+
+}
+fun fnDemo() {
+    val dt = 10
+
+    fun hi() {
+        println("dt is ${dt}")
+    }
+    val fn = fun(n1: Int , n2: Int): Int {
+        val amt = 45
+        return n1 * n2 + dt + amt
+
+    }
+    hi()
+    println(fn)
+    println(fn(10,20))
 }
 
 fun main() {
     objDemo()
+
+    fnDemo()
+
 }
