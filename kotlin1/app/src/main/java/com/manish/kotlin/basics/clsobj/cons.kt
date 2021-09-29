84426f383c688dc1ed7fa245a48d44c393292f52
manishmanu7878 <manishmanu7878@gmail.com>
2021-09-29 12:53:17 +0530

    secondary constructors

--- kotlin1/app/src/main/java/com/manish/kotlin/basics/clsobj/cons.kt
+++ kotlin1/app/src/main/java/com/manish/kotlin/basics/clsobj/cons.kt
@@ -12,6 +12,20 @@ class Cls3(nm : String) {
 class Cls4(val name : String) {
     fun whatYourName() = name
 }
+class Cls5 {
+    val dt : String
+
+    constructor(d: String) {
+        dt = d
+    }
+}
+
+class Cls6(val dt : String) {
+    constructor(d: String,  n: Int): this(d) {
+    }
+    constructor(d: String,  n: Double): this(d) {
+    }
+}
 
 fun main() {
     val c1 = Cls3("android")
@@ -21,5 +35,12 @@ fun main() {
     println("Name is ${c2.whatYourName()}")
 
     val c3 = Cls4("again android")
-    println("Name is ${c3.whatYourName()}")
+    println("Name is ${c3.name}")
+
+    val c4 = Cls5("android")
+    println(" Dt is ${c4.dt}")
+
+    val c5 = Cls6("primary")
+    val c6 = Cls6("primary", 10)
+    val c7 = Cls6("primary", 10.6)
 }
